296d39d36d19f0e20c79e052c6c1e2dfa8b2ec6a
ByteNinjaa <ksadakah1@gmail.com>
2024-01-21 02:50:22 -0500

    test

--- new_script.gd
+++ new_script.gd
@@ -1,7 +1,8 @@
 extends Node
 
 this is a test
-# Temur
+# Temur 
+testing again
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
